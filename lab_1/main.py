280b9e06a4800f44a34f9c87426b8de2437bd6e9
maria-abr <vingilome@gmail.com>
2019-10-06 01:03:30 +0300

    Added the final version

--- lab_1/main.py
+++ lab_1/main.py
@@ -4,21 +4,98 @@ Count frequencies dictionary by the given arbitrary text
 """
 
 
-def calculate_frequences(text: str) -> dict:
+s_words = ('of', 'to', 'a', 'and')
+path_to_input_file = '../data.txt'
+path_to_output_file = '../report.txt'
+punctuation = '!"#$%&\'()*+,-./:;<=>?@[]\\^_`{|}~'
+
+
+def calculate_frequencies(text: str) -> dict:
     """
     Calculates number of times each word appears in the text
     """
-    print("Something interesting")
-    print("Or maybe not")
+    if type(text) != str:
+        return {}
+    list_of_words = text.split()
+    frequencies = {}
+    for word in list_of_words:
+        symbol_index = -1
+        while len(word) > -symbol_index and word[symbol_index] in punctuation:
+            symbol_index -= 1
+        if symbol_index < -1:
+            word = word[:symbol_index + 1]
+        symbol_index = 0
+        while len(word) > symbol_index and word[symbol_index] in punctuation:
+            symbol_index += 1
+        word = word[symbol_index:]
+        if not word.isalpha():
+            if word[-2:] == "'s":
+                word = word[:-2]
+            elif '-' in word:
+                pass
+            else:
+                continue
+        word = word.lower()
+        frequencies[word] = frequencies.get(word, 0) + 1
+    return frequencies
+
 
 def filter_stop_words(frequencies: dict, stop_words: tuple) -> dict:
     """
     Removes all stop words from the given frequencies dictionary
     """
-    pass
+    if type(stop_words) != tuple:
+        stop_words = ()
+    if type(frequencies) != dict:
+        return {}
+    filtered_dictionary = {}
+    for word in frequencies:
+        if word not in stop_words:
+            filtered_dictionary[word] = frequencies[word]
+    return filtered_dictionary
+
 
 def get_top_n(frequencies: dict, top_n: int) -> tuple:
     """
     Takes first N popular words
     """
-    pass
+    if type(top_n) != int:
+        try:
+            top_n = int(top_n)
+        except TypeError:
+            top_n = len(frequencies)
+    if type(frequencies) != dict:
+        return ()
+    if top_n > len(frequencies):
+        top_n = len(frequencies)
+    n_popular = ()
+    ordered_frequencies = sorted(frequencies.items())
+    ordered_frequencies = sorted(ordered_frequencies, key=lambda word_freq: word_freq[1], reverse=True)
+    for word_number in range(top_n):
+        n_popular += ordered_frequencies[word_number][0],
+    return n_popular
+
+
+def read_from_file(path_to_file: str, lines_limit: int) -> str:
+    if type(lines_limit) != int:
+        try:
+            lines_limit = int(lines_limit)
+        except TypeError:
+            lines_limit = 0
+    text = ''
+    with open(path_to_file, 'r') as file_input:
+        for line in range(lines_limit):
+            text += file_input.readline()
+    return text
+
+
+def write_to_file(path_to_file: str, content: tuple):
+    with open(path_to_file, 'w') as file_output:
+        for item in content:
+            file_output.write(item + '\n')
+
+
+word_frequencies = calculate_frequencies(read_from_file(path_to_input_file, 400))
+filtered_frequencies = filter_stop_words(word_frequencies, s_words)
+result = get_top_n(filtered_frequencies, 60)
+write_to_file(path_to_output_file, result)
